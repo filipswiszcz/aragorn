687ddf0bf8eaba665082bf61d337c8b1550ca0ee
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2024-08-11 23:27:24 +0200

    repo init

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#define GL_SILENCE_DEPRECATION
+#include <GLFW/glfw3.h>
+
+int main() {
+    
+    GLFWwindow* window;
+
+    if (!glfwInit()) return -1;
+
+    window = glfwCreateWindow(800, 600, "aragorn", NULL, NULL);
+    if (!window) {
+        glfwTerminate(); return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+//        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
